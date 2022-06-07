0e779462b08cfd92301023274dd90eaf37b7133c
s0ooo0k <izz_m2@naver.com>
2022-06-08 00:38:51 +0900

    [rebase] size

--- meowgame/Assets/Script/Pointer.cs
+++ meowgame/Assets/Script/Pointer.cs
@@ -6,6 +6,8 @@ using UnityEngine.EventSystems;
 public class Pointer : MonoBehaviour
 {
 
+    /*
+
     private float size = 2f;
     public float speed;
     public float time;
@@ -15,29 +17,18 @@ public class Pointer : MonoBehaviour
     private void Awake()
     {
         originScale = transform.localScale; //원래 크기 저장
-    }
-    private void OnEnable()
-    {
-        StartCoroutine(Up());
-    }
-    IEnumerator Up()
-    {
-        while (transform.localScale.x < size)
-        {
-            transform.localScale = originScale * (1f + time * speed);
-            time += Time.deltaTime;
+    }   
 
-            if (transform.localScale.x >= size)
-            {
-                time = 0;
-                break;
-            }
-            yield return null;
-        }
+
+    public void Size(){
+        transform.localScale = originScale*(1+time);
+        time += Time.deltaTime;
+    }
+    */
+    Vector3 scaleChange = new Vector3(0.1f, 0.1f, 0.1f);
+    private GameObject selectedObject;
+    public void Size(){
+        selectedObject.transform.localScale += scaleChange;
     }
 
-    private void OnDisable()
-    {
-        gameObject.transform.localScale = originScale;
-    }
 }
