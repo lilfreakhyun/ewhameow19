f41c9692a3a731b588a2a546ac47df7f4e3523ea
jhoh <jihyuno0730@naver.com>
2022-06-08 21:35:53 +0900

    0608 semi 09:34

---
+++ meowgame/Assets/Script/HeartPointer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class HeartPointer : MonoBehaviour
+{
+    public Image imgGaze;
+    public UnityEvent GVRClick;
+    public float totalTime;
+    bool gvrStatus;
+    public float gvrTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gvrStatus)
+        {
+            gvrTimer += Time.deltaTime;
+            imgGaze.fillAmount = gvrTimer / totalTime;
+
+        }
+    
+           if (gvrTimer > totalTime)
+        {
+            GVRClick.Invoke();
+        }
+  
+    }
+
+    public void GVROn()
+    {
+        gvrStatus = true;
+    }
+
+    public void GVROff()
+    {
+        gvrStatus = false;
+        gvrTimer = 0;
+        imgGaze.fillAmount = 0;
+}
+}
+
+  
